b1e5579f1ae192406a8f1a5541ed0350c2bd61ff
JiyeSun <jiyesun217@gmail.com>
2025-04-03 00:36:18 -0400

    Update rag_helper.py

--- rag_helper.py
+++ rag_helper.py
@@ -1,7 +1,6 @@
 
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
-from llama_index.readers.web import SimpleWebPageReader
-
+from llama_index.readers.web_page import SimpleWebPageReader
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
 def build_knowledge_index(pdf_folder="data", urls=None, save_path="my_knowledge_index"):
     if urls is None:
         urls = []
@@ -13,7 +12,7 @@ def build_knowledge_index(pdf_folder="data", urls=None, save_path="my_knowledge_
         web_docs = web_loader.load_data(urls)
 
     docs = pdf_docs + web_docs
-    index = VectorStoreIndex.from_documents(docs)
+    index = GPTVectorStoreIndex.from_documents(docs)
     index.storage_context.persist(save_path)
     print(f"Index built and saved to: {save_path}")
 
@@ -24,3 +23,4 @@ def get_knowledge_context(query, index_path="my_knowledge_index", top_k=3):
     retrieved_docs = retriever.retrieve(query)
     context = "\n".join([doc.text for doc in retrieved_docs])
     return context
+
